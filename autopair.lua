bf79ea2ff3ab1a9902f6912c4334e2757a31b679
rafael <rafael@rafael-pc.localdomain>
2022-05-11 11:53:52 -0300

    Plugins added.

---
+++ autopair.lua
@@ -0,0 +1,59 @@
+-- Simple configuration script to autopair "", '', (), [] and {}
+-- By Rafael Fernandes, 2022. Public Domain.
+
+-- bring some functions to local scope
+local f = string.format
+local t = vim.api.nvim_replace_termcodes
+local getcursor = vim.api.nvim_win_get_cursor
+local buftext = vim.api.nvim_buf_get_text
+
+-- get next character of the cursor
+local function nextchar()
+	local row, col = unpack(getcursor(0))
+	row = row - 1 -- change from 1 indexed to 0 indexed
+	return buftext(0, row, col, row, col + 1, {})[1]
+end
+
+-- cursor movement chars
+local move_prev = t('<Left>', true, true, true)
+local move_next = t('<Right>', true, true, true)
+
+-- functions for pairs with different chars
+local function pair_open(pair)
+	return f("%s%s", pair, move_prev)
+end
+
+local function pair_close(close_char)
+	return nextchar() == close_char and move_next or close_char
+end
+
+-- function for quotes
+local quote_pair = function(quote_char)
+	return nextchar() == quote_char and move_next or f("%s%s%s", quote_char, quote_char, move_prev)
+end
+
+-- pair characters mapping table
+local autopairs = {
+	['"'] = { callback = function() return quote_pair('"') end; name = "DoubleQuotes"; },
+	["'"] = { callback = function() return quote_pair("'") end; name = "SingleQuotes"; },
+	['('] = { callback = function() return pair_open("()") end; name = "OpenParentesis"; },
+	[')'] = { callback = function() return pair_close(')') end; name = "CloseParentesis"; },
+	['['] = { callback = function() return pair_open("[]") end; name = "OpenSquareBracket"; },
+	[']'] = { callback = function() return pair_close(']') end; name = "CloseSquareBracket"; },
+	['{'] = { callback = function() return pair_open("{}") end; name = "OpenCurlyBracket"; },
+	['}'] = { callback = function() return pair_close('}') end; name = "CloseCurlyBracket"; },
+}
+
+-- create global functions to be called and set keymaps
+local map = vim.api.nvim_set_keymap
+local autopair_functions = {}
+local map_options = { expr = true; noremap = true }
+for lhs, v in pairs(autopairs) do
+	autopair_functions[v.name] = v.callback
+	local rhs = f('v:lua.AutoPair.%s()', v.name)
+	map("i", lhs, rhs, map_options)
+end
+
+-- export autopair functions to global scope
+_G["AutoPair"] = autopair_functions
+
---
+++ statusline.lua
@@ -0,0 +1,60 @@
+-- Simple configuration script to configure statusline.
+-- By Rafael Fernandes, 2022. Public Domain.
+
+-- bring global functions to local scope
+local f = string.format
+local b = string.byte
+
+-- mode display
+local modes = {
+	[ b'n' ] = { text = "NORMAL"; color = "StatusLineNC"; },
+	[ b'v' ] = { text = "VISUAL"; color = "DiffChange"; },
+	[ b'V' ] = { text = "V-LINE"; color = "DiffChange"; },
+	[ b's' ] = { text = "SELECT"; color = "PMenuSel"; },
+	[ b'S' ] = { text = "S-LINE"; color = "PMenuSel"; },
+	[ b'i' ] = { text = "INSERT"; color = "CursorLineNr"; },
+	[ b'R' ] = { text = "REPLACE"; color = "DiffDelete"; },
+	[ b'c' ] = { text = "COMMAND"; color = "Question"; },
+	[ b'r' ] = { text = "PROMPT"; color = "Question"; },
+	[ b't' ] = { text = "TERMINAL"; color = "Terminal"; },
+	[ b'!' ] = { text = "RUNNING"; color = "IncSearch"; },
+	[b'\22'] = { text = "V-BLOCK"; color = "DiffChange"; }, -- CTRL-V
+	[b'\19'] = { text = "S-BLOCK"; color = "PMenuSel"; }, -- CTRL-S
+}
+
+local function curmode()
+	local m = modes[b(vim.api.nvim_get_mode().mode)]
+	return m and f("%%#%s# %s ", m.color, m.text) or ""
+end
+
+-- file type, format and encoding
+local function fileinfo()
+	local bufn = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local format = vim.api.nvim_buf_get_option(bufn, "fileformat")
+	local encoding = vim.api.nvim_buf_get_option(bufn, "fileencoding")
+	local type = vim.api.nvim_buf_get_option(bufn, "filetype")
+	local info = f("%%#StatusLineNC# %s │ %s │ %s⏎ ", type, encoding, format)
+	return string.gsub(info, "  │", "") -- remove empty info
+end
+
+-- ruler with scroll
+local scroll = { "▕██▏", "▕▇▇▏", "▕▆▆▏", "▕▅▅▏", "▕▄▄▏", "▕▃▃▏", "▕▂▂▏", "▕▁▁▏", "▕  ▏" }
+
+local function make_ruler()
+	local bufn = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local pos = vim.fn.line('.', vim.g.statusline_winid)
+	local total = vim.api.nvim_buf_line_count(bufn)
+	local c = math.ceil((pos / total) * #scroll)
+	return f("%%#CursorLineNr# %%12(%%l:%%2.v%s%%)", scroll[c])
+end
+
+-- setup statusline function
+_G["statusline"] = function()
+	local active = vim.g.statusline_winid == vim.api.nvim_get_current_win()
+	local mode = active and curmode() or "" -- do not show mode in inactive windows
+	local color = active and "StatusLine" or "StatusLineNC"
+	local name = "%(%t%< %h%w%r%m%)"
+	return f("%s%%#%s# %s%%=%s%s", mode, color, name, fileinfo(), make_ruler())
+end
+vim.go.statusline = "%!v:lua.statusline()"
+
