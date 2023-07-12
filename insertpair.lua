22d35b009beef22499c0d39026a05c61bc9db484
Rafael <rafmagus@hotmail.com>
2023-07-12 17:38:35 -0300

    Fixed insertpair when visual selection included end of the line.

--- insertpair.lua
+++ insertpair.lua
@@ -4,6 +4,7 @@
 -- Import functions
 local getmarkpos = vim.fn.getpos
 local settext = vim.api.nvim_buf_set_text
+local gettext = vim.api.nvim_buf_get_text
 local map = vim.keymap.set
 local esc_key = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
 local sendkey = vim.api.nvim_feedkeys
@@ -27,8 +28,14 @@ local function add_pairs(first, last)
 		col1, col2 = col2, col1
 	end
 	row1, row2 = row1 - 1, row2 - 1 -- from 1 indexed to 0 indexed
-	col1 = col1 - 1 -- position before the visual selection
-	col2 = col2 + 1 -- position after the visual selection
+	-- position before the visual selection
+	col1 = col1 - 1
+	-- check if the "last" position is not the end of the line
+	local last_char = gettext(0, row2, col2 - 1, row2, col2, {})[1]
+	if last_char ~= "" then
+		-- not at the end of line, position it after visual selection
+		col2 = col2 + 1
+	end
 	settext(0, row1, col1, row1, col1, { first })
 	settext(0, row2, col2, row2, col2, { last })
 	sendkey(esc_key, "n", false)
@@ -36,5 +43,5 @@ end
 
 -- setup keybinding for visual mode
 for key, value in pairs(char_pairs) do
-	map("v", '<C-i>'..key, function () add_pairs(value.first, value.last) end)
+	map("v", '<Leader>' .. key, function() add_pairs(value.first, value.last) end)
 end
