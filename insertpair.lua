74a3fd61f82ca1957452f74704fc970e4729aad6
Rafael <rafmagus@hotmail.com>
2023-12-18 07:01:05 -0300

    insertpair.lua: made the logic a bit clearer.

--- insertpair.lua
+++ insertpair.lua
@@ -1,4 +1,4 @@
--- Simple configuration script to insert "", '', (), [] or {} around a selection.
+-- Simple configuration script to insert "", '', (), [] or {} around a visual selection.
 -- By Rafael Fernandes, 2023. Public Domain.
 
 -- Import functions
@@ -11,11 +11,11 @@ local sendkey = vim.api.nvim_feedkeys
 
 -- pairs table
 local char_pairs = {
-	['"'] = { first = '"', last = '"' },
-	["'"] = { first = "'", last = "'" },
-	["("] = { first = '(', last = ')' },
-	["["] = { first = '[', last = ']' },
-	["{"] = { first = '{', last = '}' },
+	['"'] = '"',
+	["'"] = "'",
+	["("] = ')',
+	["["] = ']',
+	["{"] = '}',
 }
 
 -- insert fist and last characters around selected text
@@ -30,18 +30,18 @@ local function add_pairs(first, last)
 	row1, row2 = row1 - 1, row2 - 1 -- from 1 indexed to 0 indexed
 	-- position before the visual selection
 	col1 = col1 - 1
-	-- check if the "last" position is not the end of the line
+	-- don't let col2 be past the end of the line (visual selection allows it)
 	local last_char = gettext(0, row2, col2 - 1, row2, col2, {})[1]
-	if last_char ~= "" then
-		-- not at the end of line, position it after visual selection
-		col2 = col2 + 1
+	if last_char == "" then
+		col2 = col2 - 1
 	end
-	settext(0, row1, col1, row1, col1, { first })
+	-- insert pair and leave visual selection mode
 	settext(0, row2, col2, row2, col2, { last })
+	settext(0, row1, col1, row1, col1, { first })
 	sendkey(esc_key, "n", false)
 end
 
 -- setup keybinding for visual mode
 for key, value in pairs(char_pairs) do
-	map("v", '<Leader>' .. key, function() add_pairs(value.first, value.last) end)
+	map("v", "<Leader>" .. key, function() add_pairs(key, value) end)
 end
